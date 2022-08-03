449118401df0c5b6f4cabe17143908f4f0554325
SynCROSS <kuuhaku3125@gmail.com>
2022-08-03 22:42:47 +0900

    Return New Object To Prevent Modifying Value

--- src/test/java/org/example/MoneyTest.java
+++ src/test/java/org/example/MoneyTest.java
@@ -1,13 +1,23 @@
 package org.example;
 
+import org.example.dollar.Dollar;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyTest {
     @Test
     final void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10,
+                     product.amount,
+                     "Test Failed"
+        );
+        product = five.times(3);
+        assertEquals(15,
+                     product.amount,
+                     "Test Failed"
+        );
     }
 }
