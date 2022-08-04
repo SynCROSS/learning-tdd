fd055ac242df8d6451f2c3b256656e457cf37b6d
SynCROSS <kuuhaku3125@gmail.com>
2022-08-04 13:10:24 +0900

    Add 'eqauls' Method

--- src/main/java/org/example/dollar/Dollar.java
+++ src/main/java/org/example/dollar/Dollar.java
@@ -1,5 +1,7 @@
 package org.example.dollar;
 
+import java.util.Objects;
+
 public class Dollar {
     public int amount; // Use Double Or Else
 
@@ -9,5 +11,20 @@ public class Dollar {
 
     public Dollar times(int multiplier) {
         return new Dollar(this.amount * multiplier);
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (null == obj || this.getClass() != obj.getClass())
+            return false;
+        Dollar dollar = (Dollar) obj;
+        return this.amount == dollar.amount;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(this.amount);
     }
 }
--- src/test/java/org/example/MoneyTest.java
+++ src/test/java/org/example/MoneyTest.java
@@ -4,20 +4,36 @@ import org.example.dollar.Dollar;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MoneyTest {
     @Test
     final void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10,
-                     product.amount,
-                     "Test Failed"
+        assertEquals(
+                10,
+                product.amount,
+                "product.amount should be 10"
         );
         product = five.times(3);
-        assertEquals(15,
-                     product.amount,
-                     "Test Failed"
+        assertEquals(
+                15,
+                product.amount,
+                "product.amount should be 15"
+        );
+    }
+
+    @Test
+    final void testEquality() {
+        assertTrue(
+                new Dollar(5).equals(new Dollar(5)),
+                "new Dollar(5) is equal to new Dollar(5)"
+        );
+        assertFalse(
+                new Dollar(5).equals(new Dollar(6)),
+                "new Dollar(5) is not equal to new Dollar(6)"
         );
     }
 }
